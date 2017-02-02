90ec3789e6686e6486631a5fa354ac268038fea9
Parshant Micoo <pmicoo@hotmail.com>
2017-02-02 20:18:36 +0000

    Seeded DB

--- db/seeds/restaurant_seed.js
+++ db/seeds/restaurant_seed.js
@@ -1,11 +1,54 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('restaurant').del()
-    .then(function () {
-      return Promise.all([
-        // Inserts seed entries
-        knex('restaurant').insert({name: 'Carol\'s Cupcakes'})
-      ]);
-    });
+  return knex('products').del()
+  .then(function() {
+    return knex('restaurant').del()
+      .then(function() {
+          return knex('restaurant').insert({name: 'Carol\'s Cupcakes'})
+            .then(function() {
+              return knex('products').insert({
+                restaurant_id: 1,
+                name: 'Chocolate Cupcake',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.',
+                price: 2.99
+              });
+            }).then(function() {
+                return knex('products').insert({
+                  restaurant_id: 1,
+                  name: 'Vanilla Cupcake',
+                  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.',
+                  price: 1.99
+                });
+            }).then(function() {
+                return knex('products').insert({
+                  restaurant_id: 1,
+                  name: 'Blueberry Cupcake',
+                  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.',
+                  price: 2.49
+                });
+            }).then(function() {
+                return knex('products').insert({
+                  restaurant_id: 1,
+                  name: 'Raspberry Cupcake',
+                  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.',
+                  price: 2.49
+                });
+            }).then(function() {
+                return knex('products').insert({
+                  restaurant_id: 1,
+                  name: 'Extreme Chocolate Cupcake',
+                  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.',
+                  price: 3.99
+                });
+            }).then(function() {
+                return knex('products').insert({
+                  restaurant_id: 1,
+                  name: 'Cinnamon Cupcake',
+                  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.',
+                  price: 2.99
+                });
+            });
+      });
+  });
 };
