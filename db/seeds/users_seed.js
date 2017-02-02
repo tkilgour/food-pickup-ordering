c4fcfc446c8224c45d9a4f6f1b66504a74e2a8c0
Parshant Micoo <pmicoo@hotmail.com>
2017-02-02 18:14:53 +0000

    knex seeding

--- db/seeds/users_seed.js
+++ db/seeds/users_seed.js
@@ -3,9 +3,6 @@ exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('users').del()
     .then(function () {
-      return Promise.all([
-        // Inserts seed entries
-        knex('users').insert({first_name: 'James', last_name: 'Bond', phone: '14165555555'})
-      ]);
+      return knex('users').insert({first_name: 'James', last_name: 'Bond', phone: '14165555555'})
     });
 };
