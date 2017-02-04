162e5b71d95e134758ca3b851b0e4c2421077db1
Parshant Micoo <pmicoo@hotmail.com>
2017-02-04 17:53:21 +0000

    added columns to orders and fully seeded DB

--- db/seeds/users_seed.js
+++
@@ -1,8 +0,0 @@
-
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('users').del()
-    .then(function () {
-      return knex('users').insert({first_name: 'James', last_name: 'Bond', phone: '14165555555'})
-    });
-};
