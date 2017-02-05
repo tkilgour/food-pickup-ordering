7f381f13fd7b364537e667c61697a49736040cda
Parshant Micoo <pmicoo@hotmail.com>
2017-02-05 15:47:38 +0000

    route logic

--- public/scripts/order-status.js
+++ public/scripts/order-status.js
@@ -1,4 +1,28 @@
 
 $(document).ready(function () {
-  $('.oid').attr('data-id', locals.userOrders[i].id);
+
+  $('.oid').on('submit', function(event) {
+    event.preventDefault();
+
+    let id = $(this).data('id');
+    let val = Number($(this).find('.txt-input').val());
+
+    const timeData = {
+      id: id,
+      val: val
+    }
+
+    $.ajax('order_status', {
+        method: 'post',
+        data: timeData
+    })
+    .then(function() {
+      $('.txt-input').val('');
+      //$.ajax({url: '/admin/order_status'});
+    })
+    .fail(function(error) {
+      //display any errors
+        console.error(error);
+      });
+  });
 });
