6ba118a988bccf9ef479f7e6aa258c4f3c821b07
Parshant Micoo <pmicoo@hotmail.com>
2017-02-05 19:06:18 +0000

    seed fixed

--- db/migrations/20170204160612_orders_table_update.js
+++ db/migrations/20170204160612_orders_table_update.js
@@ -10,7 +10,7 @@ exports.up = function(knex, Promise) {
 exports.down = function(knex, Promise) {
   return knex.schema.table('orders', function(table) {
     table.dropColumn('time');
-    table.dropColumn('date_Created');
+    table.dropColumn('date_created');
     table.dropColumn('complete');
   });
 };
